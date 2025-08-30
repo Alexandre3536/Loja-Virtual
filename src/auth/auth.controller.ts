d1ba6e4ecd3c4cf0a96554c063b552d4ad3071f4
Alexandre De Bortoli <alexandre.debortoli26@gmail.com>
2025-08-30 20:05:53 -0300

    corrigir rota usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Usuario } from '../usuario/usuario.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -7,10 +8,19 @@ export class AuthController {
 
   @Post('login')
   async login(@Body() dadosLogin: { email: string; senha: string }) {
-    const usuario = await this.authService.validateUsuario(dadosLogin.email, dadosLogin.senha);
+    const usuario = await this.authService.validateUsuario(
+      dadosLogin.email,
+      dadosLogin.senha,
+    );
     if (!usuario) {
       return { error: 'Credenciais inválidas' };
     }
+    return this.authService.login(usuario);
+  }
+
+  @Post('cadastro')
+  async cadastro(@Body() usuarioData: Partial<Usuario>) {
+    const usuario = await this.authService.register(usuarioData);
     return this.authService.login(usuario);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, InternalServerErrorException } from '@nestjs/common';
 import { UsuarioService } from '../usuario/usuario.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Usuario } from 'src/usuario/usuario.entity';
 
 @Injectable()
 export class AuthService {
@@ -24,5 +25,29 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async register(usuarioData: Partial<Usuario>): Promise<Usuario> {
+    if (!usuarioData.email) {
+      throw new InternalServerErrorException('E-mail do usuário não fornecido.');
+    }
+    
+    const usuarioExistente = await this.usuariosService.buscarPorEmail(
+      usuarioData.email,
+    );
+
+    if (usuarioExistente) {
+      throw new UnauthorizedException('Este e-mail já está em uso.');
+    }
+    
+    if (!usuarioData.senha) {
+      throw new UnauthorizedException('A senha é obrigatória para o cadastro.');
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(usuarioData.senha, salt);
+    usuarioData.senha = hashedPassword;
+
+    return this.usuariosService.criar(usuarioData.email, usuarioData.senha);
   }
 }
