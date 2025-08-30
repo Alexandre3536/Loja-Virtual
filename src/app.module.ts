6d0874b23dcca83e32b9100c885d4584a34b6f02
Alexandre De Bortoli <alexandre.debortoli26@gmail.com>
2025-08-30 20:12:22 -0300

    logim

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,27 +5,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProdutoModule } from './produto/produto.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Use apenas para desenvolvimento!
       ssl: {
         rejectUnauthorized: false,
       },
+      synchronize: true,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/uploads',
     }),
     ProdutoModule,
+    AuthModule,
+    UsuarioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
